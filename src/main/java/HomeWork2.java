919d82fba35e56bf9c1589ee0673162ef2450eb8
Samelena <lyly-miranda@yandex.ru>
2021-03-30 09:41:02 +0300

    HomeWorkLesson2

--- src/main/java/HomeWork2.java
+++ src/main/java/HomeWork2.java
@@ -6,7 +6,9 @@ public class HomeWork2 {
         isNegative(-25);
         printWordNTimes(12, 5);
     }
-
+//Написать метод, принимающий на вход два целых числа и проверяющий,
+// что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true,
+// в противном случае – false.
     public static boolean wthin10and20(int x1, int x2) {
         int y = x1 + x2;
 
@@ -16,7 +18,9 @@ public class HomeWork2 {
             return false;
         }
     }
-
+//Написать метод, которому в качестве параметра передается целое число, метод должен
+    //напечатать в консоль, положительное ли число передали или отрицательное.
+// Замечание: ноль считаем положительным числом.
     public static void isPositiveOrNegative(int x) {
         if (x >= 0) {
             System.out.println("положительное");
@@ -24,14 +28,16 @@ public class HomeWork2 {
             System.out.println("отрицательное");
         }
     }
-
+//Написать метод, которому в качестве параметра передается целое число.
+// Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
     public static boolean isNegative(int x) {
         if (x < 0) {
             return true;
         }
         return false;
     }
-
+//Написать метод, которому в качестве аргументов передается строка и число,
+// метод должен отпечатать в консоль указанную строку, указанное количество раз;
     public static void printWordNTimes(int word, int times) {
         for (int i = 0; i < times; i++) {
             System.out.print(word);
